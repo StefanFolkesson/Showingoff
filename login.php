6ba5fad781b4e463ca6c35266c3f18ebecdd04a3
StefanFolkesson <stefan.folkesson@ntig.se>
2025-03-07 13:48:27 +0100

    ollefixxar

--- login.php
+++ login.php
@@ -1,16 +1,10 @@
 <?php
 // Fånga upp post med användarnamn och lösenord
-if(isset($_POST['username']) && isset($_POST['password'])) {
-    // Hämta användarnamn och lösenord
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    // Kolla om användarnamn och lösenord är korrekt
-    if($username == 'admin' && $password == 'password') {
-        // Skapa en session
+if(isset($_POST['hannes']) && isset($_POST['password'])) {
         session_start();
         $_SESSION['username'] = $username;
         // Skicka användaren till startsidan
-        header('Location: index.php');
+        header('Location: ushlubnushlu.php');
     } else {
         // Om användarnamn och lösenord är fel
         echo 'Fel användarnamn eller lösenord';
